6bf57c38c3a53a93574b2c4960f80afbd97dcbeb
Clar3nce <clar3nce@protonmail.ch>
2019-01-23 12:34:26 +0000

    made more progress on calc including most of the basic functions i will need

--- calculator/src/calc.asm
+++ calculator/src/calc.asm
@@ -34,6 +34,32 @@ _syscall:  ; declare kernel syscall
     ret    ; return 
 
 
+_sum:
+  mov  edx, 64
+  mov  ecx, result
+  mov  ebx, 1
+  mov  eax, 0 
+  add  eax, [num_one]
+  add  eax, [num_two]
+  mov [result], eax
+
+  push dword sum_len
+  push dword sum
+  push dword 1
+  mov  eax, 0x4
+  call _syscall
+
+  push dword 64
+  push dword result
+  push dword 1
+  mov  eax, 0x4
+  call _syscall 
+
+  add  esp, 39
+  push dword 0
+  mov  eax, 0x1
+  call _syscall
+
 _info:
    push dword inf_len ; push size of info message onto stack 
    push dword info    ; push info message onto the stack
@@ -41,9 +67,6 @@ _info:
    mov  eax, 0x4      ; move 0x4 into EAX 0x4 = SYS_WRITE
    call _syscall      ; call sys_write with the values on the stack as arguments 
    add  esp, 12       ; clear the last 12 bytes from the stack 
-   push dword 0       ; push exit code 0 onto the stack
-   mov  eax, 0x1      ; move 0x1 into EAX 0x1 = SYS_EXIT
-   call _syscall      ; call SYS_EXIT by using _syscall function with EAX containing the syscall number 
 
 _read_first_number:
    push dword in_f_len ; push size of in_f messgae onto the stack
@@ -60,20 +83,21 @@ _read_first_number:
    call _syscall      ; call SYS_READ by using _syscall function with EAX containing the syscall number
    cmp  eax, 0        ; compare EAX to 0 to determine if all input data has been received
    add  esp, 15
-   jle  read_second_number ; jump to reading the second number if EAX = 0
+   jle  _read_second_number ; jump to reading the second number if EAX = 0
    
 
-   push dword in_f_len
-   push dword in_f
-   push dword 1
-   mov  eax, 0x4
-   call _syscall
-   ; continue from here 
+   push dword 32       ; push the length of the 1st number 1 buffer to the stack
+   push dword num_one  ; push the value of num_one ot stack 
+   push dword 1        ; push file descriptor to stack 
+   mov  eax, 0x4       ; move 0x4 into EAX;  0x4 = SYS_WRITE 
+   call _syscall       ; call SYS_WRITE using syscall function 
+   add  esp,12         ; clear last 12 bytes of stack
+   jmp  _read_first_number ; jump back to the start of this function and collect next value 
 
 
 _read_second_number: 
-   push dword in_f_len ; push size of in_f messgae onto the stack
-   push dword in_f     ; push in_f message onto the stack 
+   push dword in_s_len ; push size of in_s messgae onto the stack
+   push dword in_s     ; push in_s message onto the stack 
    push dword 1        ; push file descriptor onto the stack
    mov  eax, 0x4       ; move 0x4 into EAX 0x4 = SYS_WRITE
    call _syscall       ; call SYS_WRITE using syscall function 
@@ -85,8 +109,8 @@ _read_second_number:
    mov  eax, 0x3      ; move 0x3 into EAX 0x3 = SYS_READ
    call _syscall      ; call SYS_READ by using _syscall function with EAX containing the syscall number
    cmp  eax, 0        ; compare EAX to 0 to determine if all input data has been received 
-
-
+   add  esp, 15       ; clear last 15 bytes of stack 
+   jmp _sum
 
 
 
